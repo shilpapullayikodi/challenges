3147e9851abd8034aa4eab5be5d37f9b688da05e
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-01 11:37:47 +0200

    update tipcalculator

---
+++ js-basics/Tipcalculator/tipcalculator.js
@@ -0,0 +1,9 @@
+let mealCost = 50;
+const tipPercentage = 0.5;
+let tipAmount = mealCost * tipPercentage;
+
+console.log("Meal cost: ", mealCost);
+console.log("Tip percentage: ", tipPercentage);
+console.log("Tip Amount: ", tipAmount);
+let totalCost = mealCost + tipAmount;
+console.log("The total cost of your bill is: ", totalCost);
