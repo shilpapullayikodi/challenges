99f023cbf9159a795d66971b83124994189ef759
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-05 16:45:16 +0200

    funstions and parameters

--- js-basics/js-functions_functions-with-parameters-extra/js/index.js
+++ js-basics/js-functions_functions-with-parameters-extra/js/index.js
@@ -6,7 +6,10 @@ console.clear();
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
+function printSquare(number) {
+  const square = number * number;
+  console.log(square);
+}
 
 printSquare(3);
 printSquare(5);
@@ -16,11 +19,14 @@ printSquare(5);
    the circumference to the console. The function does not exist yet.
    The formula is: circumference = 2 * Pi * radius
 */
+function printCircumference(radius) {
+  const circumference = 2 * Math.PI * radius;
+  console.log("circumference is: ", circumference.toFixed(2));
+}
+printCircumference(4);
+printCircumference(6);
 
 // Uncomment the following function calls and implement the function printCircumference
-
-// printCircumference(4);
-// printCircumference(6);
 
 /*
 3: We want to use a function which accepts the width and length of a rectangle
@@ -32,5 +38,9 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function printRectangleArea
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
+function printRectangleArea(width, length) {
+  const area = width * length;
+  console.log("The area of the rectangle is ", area);
+}
+printRectangleArea(5, 7);
+printRectangleArea(3, 4);
