3d011f70ca4c73150c9bc4abad4bbb6d86012938
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-05 16:22:12 +0200

    add basic function

--- js-basics/js-functions_basic-functions/js/index.js
+++ js-basics/js-functions_basic-functions/js/index.js
@@ -7,7 +7,14 @@ console.clear();
   - the rating
   - the number of sales
 */
-
+let bookData = [
+  {
+    bookTitle: "The Lord of the Javascript",
+    author: "Mario",
+    rating: 4.2,
+    numberOfSales: 120,
+  },
+];
 // --v-- write your code here --v--
 
 // --^-- write your code here --^--
@@ -23,6 +30,8 @@ Sales: 120
 Increase the number of sales and change the rating of the book afterwards.
 Then log all variables to the console again.
 */
+bookData[0].rating = 4.5;
+bookData[0].numberOfSales = 150;
 
 // --v-- write your code here --v--
 
@@ -36,6 +45,13 @@ Then log all variables to the console again.
  - Now increase the number of sales 2 more times and log the data after each increase.
 */
 
+function logBookData(bookData) {
+  console.log(bookData);
+}
+bookData[0].numberOfSales = 200;
+logBookData();
+bookData[0].numberOfSales = 1500;
+logBookData();
 // --v-- write your code here --v--
 
 // --^-- write your code here --^--
