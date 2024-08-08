cfb928d3cdaf887a69f6209c048a42036b6fe7bb
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-08 12:42:17 +0200

    update create element toast messages

--- js-basics/js-createelement_toast-messages/js/index.js
+++ js-basics/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,16 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const toastMessage = document.createElement("li");
+  toastMessage.classList.add("toast-container__message");
+  toastMessage.textContent = "I'm a toast message.";
+  toastContainer.append(toastMessage);
+
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
+
   // Exercise: Clear the stack of toast messages
 });
