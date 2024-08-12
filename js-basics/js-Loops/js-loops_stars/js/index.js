cd1cf1285cf81c3d0215583ae41144d3627fcf42
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-12 17:49:42 +0200

    fix attribute

--- js-basics/js-Loops/js-loops_stars/js/index.js
+++ js-basics/js-Loops/js-loops_stars/js/index.js
@@ -7,7 +7,7 @@ const starContainer = document.querySelector('[data-js="star-container"]');
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
   for (let i = 1; i <= 5; i++) {
-    const img = document.createElement("image");
+    const img = document.createElement("img");
     img.setAttribute("src", "./assets/star-empty.svg");
     starContainer.append(img)
   }
@@ -23,7 +23,7 @@ const starContainer = document.querySelector('[data-js="star-container"]');
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
   for (let i = 1; i <= 5; i++) {
-    const img = document.createElement("image");
+    const img = document.createElement("img");
     if (i <= filledStars) {
       img.setAttribute("src", "./assets/star-filled.svg");
     } else {
