04db67ef9952ec6bd1551e23ea6c877908aa29b2
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-12 17:45:45 +0200

    update loop-start

--- js-basics/js-Loops/js-loops_stars/js/index.js
+++ js-basics/js-Loops/js-loops_stars/js/index.js
@@ -2,13 +2,70 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+//Part 1
+/*  function renderStars() {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("image");
+    img.setAttribute("src", "./assets/star-empty.svg");
+    starContainer.append(img)
+  }
 
   //--v-- your code here --v--
 
   //--^-- your code here --^--
+} 
+  renderStars();*/
+
+//Part 2
+/*function renderStars(filledStars) {
+  // reset the star container before rerendering stars
+  starContainer.innerHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("image");
+    if (i <= filledStars) {
+      img.setAttribute("src", "./assets/star-filled.svg");
+    } else {
+      img.setAttribute("src", "./assets/star-empty.svg");
+    }
+    starContainer.append(img);
+  }
 }
 
-renderStars();
+renderStars(3); */
+
+//Part-3
+/*function renderStars(filledStars) {
+  // reset the star container before rerendering stars
+  starContainer.innerHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    if (i <= filledStars) {
+      img.setAttribute("src", "./assets/star-filled.svg");
+    } else {
+      img.setAttribute("src", "./assets/star-empty.svg");
+    }
+    starContainer.append(img);
+  }
+}
+
+renderStars(3);*/
+
+//Part 4
+function renderStars(filledStars) {
+  // reset the star container before rerendering stars
+
+  starContainer.innerHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    if (i <= filledStars) {
+      img.setAttribute("src", "./assets/star-filled.svg");
+    } else {
+      img.setAttribute("src", "./assets/star-empty.svg");
+    }
+    img.setAttribute("onclick", `renderStars(${i})`);
+    starContainer.append(img);
+  }
+}
+renderStars(1);
