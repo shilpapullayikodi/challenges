d8b892871cb39f3f6858f37bd96f2723130e86d9
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-12 16:35:02 +0200

    update js-loops_while-random-number

--- js-basics/js-Loops/js-loops_while-random-number/index.js
+++ js-basics/js-Loops/js-loops_while-random-number/index.js
@@ -8,7 +8,11 @@ let count = 0;
 // --v-- write/change code here --v--
 
 // --^-- write/change code here --^--
-
+while (number <= 0.9) {
+  count++;
+  console.log("The number is:", number);
+  number = Math.random();
+}
 console.log(
   `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
 );
