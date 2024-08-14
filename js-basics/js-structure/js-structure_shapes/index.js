e10d4fe34997e2d5ab7a3f5f8d806db5edd6e254
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-14 17:49:15 +0200

    modify structure-shapes

--- js-basics/js-structure/js-structure_shapes/index.js
+++ js-basics/js-structure/js-structure_shapes/index.js
@@ -3,10 +3,4 @@ import { getRandomColor } from "./utils/randomColor.js";
 import Circle from "./components/Circle/Circle.js";
 import Square from "./components/Square/Square.js";
 import Pentagon from "./components/Pentagon/Pentagon.js";
-const root = document.getElementById("root");
-const circleElement = Circle();
-const pentagonElement = Pentagon();
-const squareElement = Square();
-root.append(circleElement, squareElement, pentagonElement);
-
-//root.append(circle, square, pentagon);
+root.append(Circle(), Square(), Pentagon());
