accdc101d7496d672d8e1a223fa10a21e882d202
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-28 11:48:56 +0200

    update arrays fruits

--- react-js/React_Array/react-with-arrays_fruits/src/App.jsx
+++ react-js/React_Array/react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,20 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
-
+  const fruits = [
+    // create array of 5 fruits
+    { id: 1337, name: "🍌 Banana", color: "yellow" },
+    { id: 1340, name: "🍇 Grape", color: "purple" },
+    { id: 110, name: "🍊 Orange", color: "orange" },
+    { id: 106, name: "🍎 Apple", color: "red" },
+    { id: 1339, name: "🍍 Pineapple", color: "orange" },
+  ];
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
 }
