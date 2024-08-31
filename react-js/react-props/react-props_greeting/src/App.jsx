dd365520eb575a2ff713883456cc5e94fa725c5a
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-08-31 19:47:54 +0200

    Fix syntax

--- react-js/react-props/react-props_greeting/src/App.jsx
+++ react-js/react-props/react-props_greeting/src/App.jsx
@@ -2,17 +2,16 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      {" "}
       <Greeting name="Nila" />
-      <Greeting name="Shilpa" />;{" "}
+      <Greeting name="Shilpa" />
     </>
   );
 }
 export function Greeting({ name }) {
   const coaches = ["Nila", "Nirmal"];
   if (coaches.includes(name)) {
-    return <h1>`Hello Coach! ${name}`</h1>;
+    return <h1>Hello Coach {name}</h1>;
   } else {
-    return <h1>`Hello ${name}`</h1>;
+    return <h1>Hello {name}</h1>;
   }
 }
