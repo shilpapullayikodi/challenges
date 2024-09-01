8b121c23d0c79e3e37d9f180de35bde347afc3d7
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-09-01 16:07:06 +0200

    Complete fruits challenge

--- react-js/React_Array/react-with-arrays_fruits/src/App.jsx
+++ react-js/React_Array/react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,41 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍎 Apple",
+      color: "red",
+    },
+    {
+      id: 100,
+      name: "🥝 Kiwi",
+      color: "green",
+    },
+
+    {
+      id: 1339,
+      name: "🍒 Cherry",
+      color: "red",
+    },
+    {
+      id: 138,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map(({ id, name, color }) => (
+        <Card key={id} name={name} color={color} />
+      ))}
     </div>
   );
 }
