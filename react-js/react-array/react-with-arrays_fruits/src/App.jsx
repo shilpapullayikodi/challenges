dd7b89a3aef0f9a0676cef153c067904baa4cd9f
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-09-05 11:53:38 +0200

    initial commit

--- react-js/react-array/react-with-arrays_fruits/src/App.jsx
+++ react-js/react-array/react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,44 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍎 Apple",
+      color: "red",
+    },
+    {
+      id: 1339,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+    {
+      id: 1340,
+      name: "🍒 Cherry",
+      color: "red",
+    },
+    {
+      id: 1341,
+      name: "🥝 Kiwi",
+      color: "green",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            <Card name={fruit.name} color={fruit.color} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
