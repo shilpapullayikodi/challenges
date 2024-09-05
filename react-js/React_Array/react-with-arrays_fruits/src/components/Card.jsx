ff2fc39f41135a600cb74af18cf3a88eed59e41e
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-09-05 11:29:37 +0200

    initial commit

--- react-js/React_Array/react-with-arrays_fruits/src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-import "./Card.css";
-
-// export default function Card({ name }) {
-//   return <p className="card">{name}</p>;
-// }
-export default function Card({ key, name, color }) {
-  return <p className={`card card--${color}`}>{name}</p>;
-}
