44a4a1b986eec2393ff982bd4d3c23cdce1f5fcd
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-09-06 12:42:25 +0200

    update Box challenge

--- React_Styled_Components/react-styled-components_box/pages/index.js
+++ React_Styled_Components/react-styled-components_box/pages/index.js
@@ -1,10 +1,14 @@
+import StyledBox from "@/components/BoxWithStyledComponents.js";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
-
+import styled from "styled-components";
+//import BoxWithStyledComponets from "../components/BoxWithStyledComponents.js";
 export default function HomePage() {
   return (
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <StyledBox />
+      <StyledBox $isBlack />
     </div>
   );
 }
