9042cd431be27cb53b857f60c235c579699dce22
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-09-08 20:03:36 +0200

    add array of objects

--- next-js/global_state/react-global-state_lights/components/Light/index.js
+++ next-js/global_state/react-global-state_lights/components/Light/index.js
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
+const initial = [
+  { name: "Kitchen", id: 1, isOn: false },
+  { name: "Living-room", id: 2, isOn: false },
+  { name: "Bedroom", id: 3, isOn: false },
+  { name: "Bathroom", id: 4, isOn: false },
+  { name: "Garage", ide: 5, isOn: false },
+  { name: "Porch", ide: 6, isOn: false },
+  { name: "Garden", ide: 7, isOn: false },
+  { name: "Office", ide: 8, isOn: false },
+];
 export default function Light({ name }) {
   const [isOn, setIsOn] = useState(false);
 
