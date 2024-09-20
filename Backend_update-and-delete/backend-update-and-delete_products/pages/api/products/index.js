ceeea6d0ab7020c28522d4658164d2b046f0c818
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-09-20 08:11:27 +0200

    Initial commit

--- Backend_update-and-delete/backend-update-and-delete_products/pages/api/products/index.js
+++ Backend_update-and-delete/backend-update-and-delete_products/pages/api/products/index.js
@@ -8,16 +8,16 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+
   if (request.method === "POST") {
-    // console.log("request.body", request.body);
     try {
       const productData = request.body;
-
-      await Product.create(productData);
-
-      response.status(201).json({ status: "Product created." });
+      const product = new Product(productData);
+      await product.save();
+      return response.status(201).json({ status: "Product created." });
     } catch (error) {
-      response.status(400).json({ error: error.message });
+      console.error(error);
+      return response.status(400).json({ error: error.message });
     }
   }
 }
